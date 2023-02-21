4881d18fdc0482a5ad9f04c6295552567b460958
Angel123Lara <adjls@gmail.com>
2023-02-21 09:47:59 -0600

    check test 12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ const routes: Routes = [
   {path: "hospital",component: DirectoryHospitalComponent},
   {path: "speciality",component: DirectorySpecialityComponent},
   {path: "welcome",component: WelcomePageComponent},
+  {path: "ProyectoM4_HSBC", redirectTo: "welcome",pathMatch: "full"},
   {path: "", redirectTo: "welcome",pathMatch: "full"},
   {path: '**', component: NotFoundComponent} 
   
